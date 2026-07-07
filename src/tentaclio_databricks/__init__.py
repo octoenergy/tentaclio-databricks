b6c101d20fd9f50e09c8defbfcaf892f28f9264c
Alex Malins <github@alexmalins.com>
2026-07-07 16:27:22 +0900

    Make isort adopt black style to stop them fighting

--- src/tentaclio_databricks/__init__.py
+++ src/tentaclio_databricks/__init__.py
@@ -2,9 +2,7 @@
 
 from tentaclio import *  # noqa
 
-# fmt: off
 from .clients.databricks_client import DatabricksClient
-
 
 # Add DB registry
 DB_REGISTRY.register("databricks+thrift", DatabricksClient)  # type: ignore
--- src/tentaclio_databricks/__main__.py
+++ src/tentaclio_databricks/__main__.py
@@ -1,8 +1,6 @@
 # Helper cli to encode urls.
 
-# fmt: off
 from tentaclio.__main__ import main
-
 
 if __name__ == "__main__":
     main(prog_name="python -m tentaclio-databricks")
