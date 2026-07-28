9b6c145516fdbe7dc4011896365e6364cb65ee0d
Alex Malins <github@alexmalins.com>
2026-07-28 12:17:37 +0900

    Fix issues highlighted by Copilot
    Make release python version deterministic

--- src/tentaclio_databricks/__init__.py
+++ src/tentaclio_databricks/__init__.py
@@ -1,8 +1,12 @@
-"""This package implements the tentaclio databricks+pyodbc database client"""
+"""This package implements the tentaclio databricks database client"""
+
+from importlib.metadata import version as _version
 
 from tentaclio import *  # noqa
 
 from .clients.databricks_client import DatabricksClient
+
+__version__ = _version("tentaclio-databricks")
 
 # Add DB registry
 DB_REGISTRY.register("databricks+thrift", DatabricksClient)  # type: ignore
