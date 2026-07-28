1a2cde75e05b1f6708106994435cb6ee3a76e685
Alex Malins <github@alexmalins.com>
2026-07-28 12:34:54 +0900

    Allow package to be imported when not installed

--- src/tentaclio_databricks/__init__.py
+++ src/tentaclio_databricks/__init__.py
@@ -1,12 +1,16 @@
 """This package implements the tentaclio databricks database client"""
 
+from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _version
 
 from tentaclio import *  # noqa
 
 from .clients.databricks_client import DatabricksClient
 
-__version__ = _version("tentaclio-databricks")
+try:
+    __version__ = _version("tentaclio-databricks")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 # Add DB registry
 DB_REGISTRY.register("databricks+thrift", DatabricksClient)  # type: ignore
